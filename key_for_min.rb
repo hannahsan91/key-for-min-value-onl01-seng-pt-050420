b8cfba95703ecd2b47551c682cdae354dac458f9
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-14 02:39:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  hash = {:phone => 999, :computer => 1200, :monitor => 6000}
+  key_for_min_value(hash)
+end
 end
